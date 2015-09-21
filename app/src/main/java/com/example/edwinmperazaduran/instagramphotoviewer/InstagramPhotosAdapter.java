e4a3e30c854d17c8d10995dbac37205f97428fbe
Edwin Peraza <edwinperaza87@gmail.com>
2015-09-20 19:07:39 -0700

    Improving Performance with the ViewHolder Pattern

--- app/src/main/java/com/example/edwinmperazaduran/instagramphotoviewer/InstagramPhotosAdapter.java
+++ app/src/main/java/com/example/edwinmperazaduran/instagramphotoviewer/InstagramPhotosAdapter.java
@@ -15,7 +15,17 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
-    //
+    private static class ViewHolder{
+        TextView tvUsername;
+        TextView tvTimeStamp;
+        TextView tvNumlikes;
+        TextView tvCaptionUsername;
+        TextView tvCaption;
+        TextView tvComments;
+        TextView tvMoreComments;
+        ImageView ivPhoto;
+        ImageView ivPhotoProfile;
+    }
 
     public InstagramPhotosAdapter(Context context, List<InstagramPhoto> objects) {
         super(context, android.R.layout.simple_list_item_1 , objects);
@@ -25,61 +35,67 @@ public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
     public View getView(int position, View convertView, ViewGroup parent) {
         InstagramPhoto photo = getItem(position);
 
+        ViewHolder viewHolder;
+
         if (convertView == null){
+            viewHolder = new ViewHolder();
             //create a new view
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo,
                     parent, false);
+            viewHolder.tvUsername  = (TextView) convertView.findViewById(R.id.tvUsername);
+            viewHolder.tvTimeStamp = (TextView) convertView.findViewById(R.id.tvTimeStamp);
+            viewHolder.tvNumlikes  = (TextView) convertView.findViewById(R.id.tvNumlikes);
+            viewHolder.tvCaptionUsername = (TextView) convertView.findViewById(R.id.tvCaptionUsername);
+            viewHolder.tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+            viewHolder.tvComments = (TextView) convertView.findViewById(R.id.tvComments);
+            viewHolder.tvMoreComments = (TextView) convertView.findViewById(R.id.tvMoreComments);
+            viewHolder.ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+            viewHolder.ivPhotoProfile = (ImageView) convertView.findViewById(R.id.ivPhotoProfile);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-        TextView tvUsername  = (TextView) convertView.findViewById(R.id.tvUsername);
-        TextView tvTimeStamp = (TextView) convertView.findViewById(R.id.tvTimeStamp);
-        TextView tvNumlikes  = (TextView) convertView.findViewById(R.id.tvNumlikes);
-        TextView tvCaptionUsername = (TextView) convertView.findViewById(R.id.tvCaptionUsername);
-        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
-        TextView tvComments = (TextView) convertView.findViewById(R.id.tvComments);
-        TextView tvMoreComments = (TextView) convertView.findViewById(R.id.tvMoreComments);
-        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
-        ImageView ivPhotoProfile = (ImageView) convertView.findViewById(R.id.ivPhotoProfile);
 
         String strTimestamp = DateUtils.getRelativeTimeSpanString(photo.getCreatedTime() * 1000,
                 System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS,
                 DateUtils.FORMAT_ABBREV_RELATIVE).toString();
 
-        tvUsername.setText(photo.getUsername());
-        tvTimeStamp.setText(strTimestamp);
-        tvNumlikes.setText("\u2764 " + String.valueOf(photo.getLikesCount()));
-        tvCaptionUsername.setText(photo.getUsername());
-        tvCaption.setText(photo.getCaption());
+        viewHolder.tvUsername.setText(photo.getUsername());
+        viewHolder.tvTimeStamp.setText(strTimestamp);
+        viewHolder.tvNumlikes.setText("\u2764 " + String.valueOf(photo.getLikesCount()));
+        viewHolder.tvCaptionUsername.setText(photo.getUsername());
+        viewHolder.tvCaption.setText(photo.getCaption());
         //Show "More Comments" if CommentCount > 2
         if (photo.getCommentCount() > 2){
-            String htmlTextCountComments = "<b>View all " + photo.getCommentCount() +  " comments </b>";
-            tvMoreComments.setText(Html.fromHtml(htmlTextCountComments));
+            String htmlTextCountComments = "View all " + photo.getCommentCount() +  " comments";
+            viewHolder.tvMoreComments.setText(Html.fromHtml(htmlTextCountComments));
         }
         //If commentCount 0 or 1 hide tvMoreComments
         //Otherwise show last 2 comments
         switch (photo.getCommentCount()){
             case 0:
             case 1:
-                tvMoreComments.setVisibility(View.GONE);
+                viewHolder.tvMoreComments.setVisibility(View.GONE);
                 break;
             default: {
                 //Insert Comments
                 String htmlTextComments = "";
                 for (int i = 0; i < photo.getComments().size() && i < 2; i++) {
                     InstagramComment comment = (InstagramComment) photo.comments.get(i);
-                    htmlTextComments += "<b><font color=\"#287AA9\">" + comment.commentUserName
-                            + "</font></b> " + comment.text + "<br>";
+                    htmlTextComments += "<b><font color=\"#084B8A\">" + comment.commentUserName
+                            + " </font></b><font color=\"#000000\">" + comment.text + "</font><br>";
                 }
-                tvComments.setText(Html.fromHtml(htmlTextComments));
+                viewHolder.tvComments.setText(Html.fromHtml(htmlTextComments));
                 break;
             }
         }
         //Insert de image using picasso
-        ivPhoto.setImageResource(0);
-        Picasso.with(getContext()).load(photo.getImageUrl()).placeholder(R.drawable.placeholder).into(ivPhoto);
+        viewHolder.ivPhoto.setImageResource(0);
+        Picasso.with(getContext()).load(photo.getImageUrl()).placeholder(R.drawable.placeholder).into(viewHolder.ivPhoto);
         //Insert Profile Picture using picasso
-        ivPhotoProfile.setImageResource(0);
+        viewHolder.ivPhotoProfile.setImageResource(0);
         Picasso.with(getContext()).load(photo.getProfileUrl()).transform(new CircleTransform())
-                .into(ivPhotoProfile);
+                .into(viewHolder.ivPhotoProfile);
         return convertView;
     }
 }
